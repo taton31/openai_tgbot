005f0cabc083ce66f0860bc801e1ea9abd470fe0
taton31 <sima9777@yandex.ru>
2023-02-05 22:41:45 +0300

    s

--- openai/AI.py
+++ openai/AI.py
@@ -39,7 +39,7 @@ def send_stat(message):
 def send_stat(message):
     ID = message.id
     bot.send_message(message.chat.id,f'qbittorrent-nox started', reply_to_message_id=ID)
-    subprocess.run(f'''qbittorrent-nox''',capture_output = True)
+    subprocess.run(f'''qbittorrent-nox &''',capture_output = True)
 
 @bot.message_handler(func=lambda message: message.chat.type == 'private' and message.text[:7] == 'magnet:')
 def torrent(message):
