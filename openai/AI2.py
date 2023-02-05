189aaa437204c3ea72e4026ea7e3ef64070410ef
taton31 <sima9777@yandex.ru>
2023-02-05 23:01:00 +0300

    fs

---
+++ openai/AI2.py
@@ -0,0 +1,11 @@
+import re
+import telebot
+import openai
+import subprocess
+
+
+bot = telebot.TeleBot("6048136076:AAGnrR8lEUit3UDwYzJnQPhcabdtm4m495g")
+# 5149682661:AAFYq2BpHTSfIYrU2wjKfUT8zn4aDe_1FIU mstr bot
+# 2001307240:AAE9UoP6z7m5oYujHoOWWx47Y9Vt_Mm-hrI test bot 
+# /home/kvout/desktop/telebot_chatGPT/openai_tgbot/openai/stat.txt
+bot.send_message('1723464345','asdf')
