31446905cb3e8c543e5a693395777c90ab614f59
taton31 <sima9777@yandex.ru>
2023-02-05 23:22:00 +0300

    f

--- openai/send_tor_info.py
+++ openai/send_tor_info.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot("6048136076:AAGnrR8lEUit3UDwYzJnQPhcabdtm4m495g")
 # 5149682661:AAFYq2BpHTSfIYrU2wjKfUT8zn4aDe_1FIU mstr bot
 # 2001307240:AAE9UoP6z7m5oYujHoOWWx47Y9Vt_Mm-hrI test bot 
 # /home/kvout/desktop/telebot_chatGPT/openai_tgbot/openai/stat.txt
-name = sys.argv[1:]
+name = " ".join(sys.argv[1:])
 chat_id = '1723464345'
 bot.send_message(chat_id, f'torrent {name} finished')
 
